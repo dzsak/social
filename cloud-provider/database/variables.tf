2fc788e19dd8007e7571acd9eef6f86310a7833a
Ádám Sisák <86593861+dzsak@users.noreply.github.com>
2023-07-05 09:13:08 +0200

    Update variables.tf

--- cloud-provider/database/variables.tf
+++ cloud-provider/database/variables.tf
@@ -1,18 +1,18 @@
-variable "database-dummy" {
+variable "database" {
   type = string
   description = "Testing database string TF variable"
   default = "dummyDB"
 }
 
-variable "is-admin-dummy" {
+variable "is-admin" {
   type = bool
   description = "Testing is-admin bool variable"
   default = true
 }
 
-variable "server_id" {
+variable "server_port" {
   type = number
   description = "Testing sensitive server id number TF variable"
   sensitive = true
-  default = 123
+  default = 1234
 }
